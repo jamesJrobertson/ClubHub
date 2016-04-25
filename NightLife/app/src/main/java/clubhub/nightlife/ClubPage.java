1dc94f346612d7884f47bbdfebe8cc36e768e850
yepach <jamesskateboarder@msn.com>
2016-04-24 18:45:31 -0600

    Revert "Revert "Various Changes""
    This reverts commit fcf74b18e26bb2f7671fcbbaf2d1e42ceb700028.

--- NightLife/app/src/main/java/clubhub/nightlife/ClubPage.java
+++ NightLife/app/src/main/java/clubhub/nightlife/ClubPage.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 /**
  * Created by James R on 2016-04-20.
@@ -16,6 +18,13 @@ public class ClubPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_page);
         listenerForRatingBar();
+        //TODO load data from server database
+        Intent intent = getIntent();
+
+        int id = intent.getExtras().getInt("ClubID");
+        Button v = (Button) findViewById(R.id.button_Promotins);
+        v.setText(String.valueOf(id));
+
     }
     public void listenerForRatingBar()
     {
